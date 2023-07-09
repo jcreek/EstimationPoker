e2ae0573c4395b4c4465d49d760607d1a6fc4afd
Josh Creek <8179928+jcreek@users.noreply.github.com>
2023-07-09 21:35:11 +0000

    feat(*): Add user indices to user list component

--- src/components/UsersList.svelte
+++ src/components/UsersList.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	export let estimates: any;
 	export let handleEstimateChange: any;
+	export let currentUserIndex: number;
 </script>
 
 <div id="users-list">
 	<h2>Users</h2>
 	<ul>
-		{#each estimates as user}
+		{#each estimates as user, index}
 			<li>
 				{user.name}
 				<input
 					type="number"
 					value={user.estimate}
 					on:input={(e) => handleEstimateChange(e, user)}
+					disabled={currentUserIndex !== index ? true : null}
 				/>
 			</li>
 		{/each}
