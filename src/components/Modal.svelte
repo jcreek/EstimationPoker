7547776a49fca677c4847544a165eec8f32859db
Josh Creek <8179928+jcreek@users.noreply.github.com>
2023-07-09 21:33:02 +0000

    feat(*): Add modal component

---
+++ src/components/Modal.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let closeModal;
+	export let joinRoom;
+
+	let userName = '';
+
+	function handleNameChange(event) {
+		userName = event.target.value;
+	}
+
+	function handleSubmit() {
+		if (userName.trim() !== '') {
+			joinRoom(userName);
+			closeModal();
+		}
+	}
+</script>
+
+<div class="modal-overlay">
+	<div class="modal">
+		<h2>Enter Your Name</h2>
+		<input type="text" placeholder="Your Name" bind:value={userName} on:input={handleNameChange} />
+		<button on:click={handleSubmit}>Save</button>
+	</div>
+</div>
+
+<style>
+	/* Styles for the modal overlay */
+	.modal-overlay {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	/* Styles for the modal content */
+	.modal {
+		background-color: #fff;
+		padding: 20px;
+		max-width: 400px;
+		border-radius: 4px;
+	}
+</style>
