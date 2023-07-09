6821e321a11f2d48c174bc744faddefc0cdcc300
Josh Creek <8179928+jcreek@users.noreply.github.com>
2023-07-09 18:21:45 +0000

    feat(*): Add room generation to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script>
-	import {generateRoomID} from './estimation/estimation.js';
+	import { generateRoomId } from './estimation/estimation.js';
+	import { goto } from '$app/navigation';
 
-	let roomID = generateRoomID();
-	console.log(roomID);
+	function startARoom() {
+		const roomId = generateRoomId();
+		goto(`/estimation/${roomId}`);
+	}
 </script>
 
 <h1>Estimation Poker</h1>
+
+<button on:click={startARoom}>Start a room</button>
