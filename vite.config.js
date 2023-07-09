027098b76237ee68f832630e6d64b7999f06f841
Josh Creek <8179928+jcreek@users.noreply.github.com>
2023-07-09 21:33:42 +0000

    build(*): Enable using server classes in frontend

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+		  allow: [
+			'/workspaces/EstimationPoker/server/classes/',
+			'/workspaces/EstimationPoker/server/classes/messages/',
+		  ],
+		},
+	  },
 });
