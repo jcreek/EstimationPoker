ba94bdb36bfbc2b5dd2d8a13b6433f885f9291c9
Josh Creek <8179928+jcreek@users.noreply.github.com>
2023-07-11 17:18:53 +0000

    feat(*): Update server for deploying

---
+++ src/server/README.md
@@ -0,0 +1,9 @@
+# Web Sockets Server
+
+## Deploying
+
+Use Docker to deploy this.
+
+Navigate to this folder and run `docker build -t estimation-ws-server .` to build the image.
+
+Run it using `docker run -it --rm -p 8080:8080 estimation-ws-server`
--- src/server/websocket.ts
+++ src/server/websocket.ts
@@ -7,7 +7,7 @@ import { ChangeEstimateMessage } from './classes/messages/ChangeEstimateMessage.
 import { SelectEstimateMessage } from './classes/messages/SelectEstimateMessage.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 function onSocketPreError(e: Error) {
 	console.log(e);
