397aa528dc09f901151c1e74c334f43f18ab0063
Josh Creek <8179928+jcreek@users.noreply.github.com>
2023-09-05 21:03:18 +0100

    feat(*): Add addsense ad to name entry

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -57,6 +57,18 @@
 			<div class="button-container">
 				<button on:click={handleSubmit}>Save</button>
 			</div>
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5812114745839139"
+				crossorigin="anonymous"></script>
+			<!-- Name Entry - Square Ad -->
+			<ins class="adsbygoogle"
+				style="display:block"
+				data-ad-client="ca-pub-5812114745839139"
+				data-ad-slot="5531796845"
+				data-ad-format="auto"
+				data-full-width-responsive="true"></ins>
+			<script>
+				(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
 		</div>
 	</div>
 </div>
